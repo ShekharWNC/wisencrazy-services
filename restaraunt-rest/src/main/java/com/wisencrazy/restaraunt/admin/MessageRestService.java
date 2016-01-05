9e7bde7f78dee2285d47270a0bc82923c0a94147
ShekharWNC <shekhar@wisencrazy.com>
2016-01-05 08:42:07 +0530

    Commiting files for persistence

--- restaraunt-rest/src/main/java/com/wisencrazy/restaraunt/admin/MessageRestService.java
+++ restaraunt-rest/src/main/java/com/wisencrazy/restaraunt/admin/MessageRestService.java
@@ -1,5 +1,8 @@
 package com.wisencrazy.restaraunt.admin;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,12 +17,23 @@ import org.slf4j.LoggerFactory;
 @Path("/message")
 public class MessageRestService {
 	
+	EntityManager em;
+	
+	
+	private void setEntityManager(){
+		EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("RestroDB");
+		em=entityManagerFactory.createEntityManager();
+		logger.info("Printing em {}",em.toString());
+        
+	}
+	
 	Logger logger=LoggerFactory.getLogger(MessageRestService.class);
     @GET
     @Path("/{param}")
     public Response printMessage(@PathParam("param") String msg) {
     	logger.error("Testing the info build");
         String result = "Restful example Changed: " + msg;
+        setEntityManager();
         return Response.status(200).entity(result).build();
     }
 }
