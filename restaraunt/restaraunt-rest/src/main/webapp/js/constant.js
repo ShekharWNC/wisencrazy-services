cf68dcf7a04ebb417fada0df2221ecd92e95efa0
satkamar <ksathish0014@gmail.com>
2016-01-21 19:11:17 +0000

    all API without test

--- restaraunt/restaraunt-rest/src/main/webapp/js/constant.js
+++ restaraunt/restaraunt-rest/src/main/webapp/js/constant.js
@@ -5,9 +5,9 @@ var CONSTANTS = (function(){
     {
         //Common API's
         this.SEARCHNEARBY = HOST + "/restaraunt-rest-0.0.1.local-SNAPSHOT/rest/restaraunt/search/nearby";
-        this.ONLINE_TEST = HOST +"/onlintest";
-        this.REGISTERAPP = HOST +"/registerApp";
-        this.GET_GALLERY_IMAGES = HOST +"/getImage?appSid=";
+        this.CITYBYSTATENAME = HOST +"/restaraunt-rest-0.0.1.local-SNAPSHOT/rest/restaraunt/cities/state/";
+        this.AREABYCITY = HOST +"/restaraunt-rest-0.0.1.local-SNAPSHOT/rest/restaraunt/areas/city/";
+        this.SIGNUPWITHGOOGLE = HOST +"/restaraunt-rest-0.0.1.local-SNAPSHOT/rest/customer/signup";
         
         return this;
     })();
@@ -18,17 +18,14 @@ var CONSTANTS = (function(){
     {
         //Common Screens
         this.HEADER = HOST + "/web/pages/header.html";
-        this.IMAGEUPLOAD = HOST + "/web/pages/upload.html";
-        this.GALLERY = HOST + "/web/pages/gallery.html";
-        this.SETACTION = HOST + "/web/pages/setaction.html";
-        this.DEMO = HOST + "/web/pages/demo.html";
-
+        
         return this;
     })();
     
     this.MESSAGES = (function(){
         this.INTERNAL_ERROR = "Oops! Internal error occured, Please try after sometime";
         this.IN_PROGRESS = "Please wait for  the momoment";
+        this.STATENOTFOUND="Please allow to access your location";
         return this;
     })();
     
--- restaraunt/restaraunt-rest/src/main/webapp/js/root.js
+++ restaraunt/restaraunt-rest/src/main/webapp/js/root.js
@@ -2,6 +2,19 @@ var app=angular.module("shop",[]);
 app.controller("HomeController",function($scope,$rootScope){
 	$scope.initAll=function(){
 		$scope.selectedTab = "login";
+		$scope.signUp={
+			"sid": null,
+			"id": null,
+			"email": null,
+			"password": null,
+			"primaryContact": null,
+			"name": null,
+			"accountStatus": null,
+			"signupType": "restaraunt",
+			"isShow": false,
+			"show": false,
+			"googleAccessToken":null
+		  }
 		$scope.date = Date('hh');
 		 $scope.getLocation();
 		 $scope.restaurantData={"restaurantName":"Jiyo Natural","imgPath":"../img/food.jpg","ratings":"2","lastReview":"Food is good"};
@@ -18,35 +31,7 @@ app.controller("HomeController",function($scope,$rootScope){
 		// }
 	}
 	
-	$scope.getLocation=function() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition($scope.showPosition);
-		} else { 
-			x.innerHTML = "Geolocation is not supported by this browser.";
-		}
-	}
-
-	$scope.showPosition=function(position) {
-	    $scope.latitudePosition= position.coords.latitude ;
-	    $scope.longitudePosition=position.coords.longitude;	
-	}
 	
-	$scope.getNearestLocation = function()
-    {
-        if ( $scope.latitudePosition && $scope.longitudePosition) {
-			$scope.axisJson={
-				"latitude": $scope.latitudePosition,
-				"longitude": $scope.longitudePosition,
-				"distance": "8",
-				"areaSid": null,
-				"stateSid": null,
-				"timings": "BR"
-			}
-			$scope.remoteCommunication(CONSTANTS.RESTAPIS.SEARCHNEARBY, "POST",$scope.axisJson, {'Content-type': 'application/json'}, null, "getNear");
-
-        }
-    }
-    
 		
 //This method used to send request and receive response over http
     $rootScope.remoteCommunication = function(requestUrl, requestMethod, data, header, optionalData, callBackOption)
@@ -74,6 +59,12 @@ app.controller("HomeController",function($scope,$rootScope){
       	    case "getNear":
                 alert(data);
       	    break;
+			case "getCity":
+				
+				break;
+			case "getArea":
+				
+				break;
         }   
     }
     
@@ -89,9 +80,80 @@ app.controller("HomeController",function($scope,$rootScope){
 		    case "getNear":
 	               alert(data.errorMessage);
 		    break;
+			case "getCity":
+				
+				break;
+			case "getArea":
+				
+				break;
         }
     }
 	
+	$scope.getLocation=function() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition($scope.showPosition);
+		} else { 
+			x.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	}
+
+	$scope.showPosition=function(position) {
+	    $scope.latitudePosition= position.coords.latitude ;
+	    $scope.longitudePosition=position.coords.longitude;	
+	};
+	
+	$rootScope.googlesignup = function($scope.userData)
+    {
+       if ($scope.userData && $scope.userData.email && $scope.userData.googleAccessToken && $scope.userData.name) {
+               $scope.remoteCommunication(CONSTANTS.RESTAPIS.SIGNUPWITHGOOGLE, "POST",$scope.signUp, {'Content-type': 'application/json'}, null, "AuthenticationAction");
+       }
+    }
+    
+    $rootScope.onSignIn=function(googleUser) {
+                // Useful data for your client-side scripts:
+                var profile = googleUser.getBasicProfile();
+               
+                $scope.signUp.name=profile.getName();
+                $scope.signUp.googleAccessToken=profile.getName();
+                $scope.signUp.email= profile.getEmail();
+                $rootScope.googlesignup($scope.signUp);
+    };
+    
+	
+    googleSignInSuccess = $rootScope.onSignIn;
+	
+	$scope.getNearestLocation = function()
+    {
+        if ( $scope.latitudePosition && $scope.longitudePosition) {
+			$scope.axisJson={
+				"latitude": $scope.latitudePosition,
+				"longitude": $scope.longitudePosition,
+				"distance": "8",
+				"areaSid": null,
+				"stateSid": null,
+				"timings": "BR"
+			}
+			$scope.remoteCommunication(CONSTANTS.RESTAPIS.SEARCHNEARBY, "POST",$scope.axisJson, {'Content-type': 'application/json'}, null, "getNear");
+        }
+    };
+    
+	$scope.getCities = function(stateName)
+    {
+        if ( stateName) {
+			$scope.remoteCommunication(CONSTANTS.RESTAPIS.CITYBYSTATENAME + stateName, "GET",null, {'Content-type': 'application/json'}, null, "getCity");
+        }else{
+			alert(CONSTANTS.MESSAGES.STATENOTFOUND);
+		}
+    };
+	
+	$scope.getAreas = function(citySid)
+    {
+        if (citySid) {
+			$scope.remoteCommunication(CONSTANTS.RESTAPIS.AREABYCITY + citySid, "GET",null, {'Content-type': 'application/json'}, null, "getArea");
+        }else{
+			alert(CONSTANTS.MESSAGES.STATENOTFOUND);
+		}
+    };
 	
 	
     $scope.loadTab = function(t) {
